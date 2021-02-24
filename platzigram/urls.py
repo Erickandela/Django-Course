bbd15abeba9cddbb2dc5d7ee6f400dea5e40e29f
Erick Candela <erickandela_rocke@hotmail.com>
2021-02-24 14:57:00 -0600

    extends user model

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,4 +1,8 @@
 """ Platzigram URLs module. """
+
+#Django
+#from django.conf.urls import urls
+from django.contrib import admin
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as posts_views
@@ -6,6 +10,7 @@ from posts import views as posts_views
 
 urlpatterns = [
 
+    path('admin/', admin.site.urls),
     path('helloworld/', local_views.hello_world),
     path('sorted/', local_views.sort_integers),
     path('hi/<str:name>/<int:age>', local_views.say_hi),
