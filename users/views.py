c158fcadf214814eab626774e8a2416193c596bf
Erick Candela <erickandela_rocke@hotmail.com>
2021-03-05 15:13:21 -0600

    Django Forms

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 """ Users views. """
 
 #Django
-from users.models import Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
@@ -11,11 +10,40 @@ from django.db.utils import IntegrityError
 
 #Models
 from django.contrib.auth.models import User
+from users.models import Profile
+
+#Forms
+from users.forms import ProfileForm
 
 
 def update_profile(request):
     """ Update a user's profile view. """
-    return render(request, 'users/update_profile.html')
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+
+    else:
+        form = ProfileForm()
+
+    return render(
+            request= request,
+            template_name = 'users/update_profile.html',
+            context={
+                'profile': profile,
+                'user': request.user,
+                'form': form
+            }
+            )
 
 
 def login_view(request):
