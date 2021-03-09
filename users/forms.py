f3858a910a76653786504d6a886b45af4f101b91
Erick Candela <erickandela_rocke@hotmail.com>
2021-03-08 21:13:28 -0600

    Add bootstrap on Signup validation forms

--- users/forms.py
+++ users/forms.py
@@ -11,27 +11,71 @@ class SignupForm(forms.Form):
     """ Sign up form. """
 
     username = forms.CharField(
+            label=False,
             min_length=4,
-            max_length=50)
+            max_length=50,
+            widget=forms.TextInput(
+                attrs={
+                    'placeholder': 'Username',
+                    'class': 'form-control',
+                    'required': True
+                    }
+            ))
     password = forms.CharField(
+            label=False,
             max_length=70,
-            widget=forms.PasswordInput())
+            widget=forms.PasswordInput(
+                attrs={
+                    'placeholder': 'Password',
+                    'class': 'form-control',
+                    'required': True
+                    }
+            ))
     password_confirmation = forms.CharField(
+            label=False,
             max_length=70,
-            widget=forms.PasswordInput())
+            widget=forms.PasswordInput(
+                attrs={
+                    'placeholder': 'Confirm password',
+                    'class': 'form-control',
+                    'required': True
+                    }
+            ))
 
     first_name = forms.CharField(
+            label=False,
             min_length=2,
-            max_length=50)
+            max_length=50,
+            widget=forms.PasswordInput(
+                attrs={
+                    'placeholder': 'First name',
+                    'class': 'form-control',
+                    'required': True
+                    }
+            ))
     last_name = forms.CharField(
+            label=False,
             min_length=2,
-            max_length=50)
+            max_length=50,
+            widget=forms.PasswordInput(
+                attrs={
+                    'placeholder': 'Last name',
+                    'class': 'form-control',
+                    'required': True
+                    }
+            ))
 
     email = forms.CharField(
+            label=False,
             min_length=6,
             max_length=70,
-            widget=forms.EmailInput()
-    )
+            widget=forms.EmailInput(
+                attrs={
+                    'placeholder': 'Email',
+                    'class': 'form-control',
+                    'required': True
+                }
+            ))
 
 
     def clean_username(self):
